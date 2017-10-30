55a2d2d9e104071e43d288adf24fa863d2c1b440
Sylvia Mejia <sylmejia@cobogpgp4613.global.publicisgroupe.net>
2017-10-30 15:35:02 -0500

    src folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { WarningAlert } from './WarningAlert/WarningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningAlert,
+    SuccessAlertComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
